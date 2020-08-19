628fc9dda2158c2035dedf96ddcd9eba6d55a06e
kmvern <vern3136@vandals.uidaho.edu>
2020-08-18 21:21:27 -0600

    Fixes
    orgainzed the code and adde the new NTU values to use on the U bend and solve for the 1 meter long heat exchanger.

--- diameter_calcs.py
+++ diameter_calcs.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import pandas as pd
+import tube_geometry
 
 # Below are the measurements for standard tube sizing
 
@@ -64,137 +65,32 @@ pipe_ID = [0.0078, 0.01038, 0.01388, 0.01708, 0.02248, 0.02786, 0.03666,
 org_diameters = pd.DataFrame([tube_OD_inch, tube_OD]).T
 
 
-#calculate the inner diameters of all the tube options with gauges
-'''
-The 'tube_OD2' is the outer diameter of the tubes for the gas gap pipe.
-The thickness of each gauge is subtracted from each tube_OD2 to get all inner 
-tube diameters (ID2) possible. These are the same as what's in the Excel sheet.
-'''
-for OD, n in zip(tube_OD, tube_name):
-    for t, g in zip(tube_thickness, gauge):
-        ID = OD - 2*t
-        
-        tube_ID.append(ID)
-        gauge_name.append(g)
-        OD_all.append(OD)
-        OD_name.append(n)
+Gas, LBE, Total = tube_geometry.inner_dia(tube_OD, tube_name, tube_thickness, gauge,
+                                   pipe_name, pipe_ID, pipe_OD)
 
-'''
-ID_all combines the ID2 diameters with the known inner diameters of schedule 10 pipes.
-'''
+Final = tube_geometry.gas_gap_calcs(Gas, LBE)
 
-Gas = pd.DataFrame([OD_name + pipe_name, gauge_name + pipe_name, OD_all+pipe_OD, tube_ID+pipe_ID], 
-            index = ['Gas Name','Gauge', 'OD_gas', 'ID_gas']).T
-LBE = pd.DataFrame([OD_name + pipe_name, gauge_name + pipe_name, tube_ID+pipe_ID, OD_all+pipe_OD], index = ['Pb Name','Pb Gauge','ID_Pb', 'OD_Pb']).T
-
-
-#Gas = Gas[Gas.OD_gas != 0.00635].reset_index()
-#LBE = LBE[LBE.OD_Pb != 0.13335].reset_index()
-
-df = pd.DataFrame([tube_OD + pipe_OD, tube_name+pipe_name]).T
-
-for i, k, j  in zip(LBE['OD_Pb'], LBE['Pb Name'], LBE['ID_Pb']):
-    gas_gap = (Gas['ID_gas'] - i)/2
-    gap.append(gas_gap)
-    length = np.ones(len(Gas['ID_gas']))
-    dia = i * length
-    x = [k]*len(Gas['ID_gas'])
-    name.append(x)
-    OD_Pb.append(dia)
-    ID_gas.append(Gas)
-    Pb_data.append([j]*len(Gas['ID_gas']))
-
-gap_calc = pd.DataFrame(np.concatenate(gap), columns = ['GasGap'])
-OD_lbe = pd.DataFrame(np.concatenate(OD_Pb),columns = ['OD_Pb'])
-name_pb = pd.DataFrame(np.concatenate(name), columns = ['LBE Name'])
-ID_info = pd.concat(ID_gas, ignore_index = True)
-Pb_info = pd.DataFrame(np.concatenate(Pb_data), columns = ['ID_Pb'])
-
-Final = pd.concat([ID_info, gap_calc, OD_lbe, Pb_info, name_pb], axis = 1)
-
-#gas gap options
+#gas gap options within 0.2 and 0.25 mm
 Final = Final[Final.GasGap >= 0.0002]
 Final = Final[Final.GasGap <= 0.00026]
-
-Total = pd.DataFrame([OD_name+pipe_name, OD_all+pipe_OD, tube_ID+pipe_ID, gauge_name + pipe_name],
-                     index =  ['Name', 'Outer', 'Inner', 'Gauge']).T
  
-#gas gap names
-
-def area_calcs(ID_pb, OD_pb, ID_gas, OD_gas, ID_w):
-    T_1 = (OD_pb - ID_pb)/2
-    T_2 = (OD_gas - ID_gas)/2
-    A_pb = math.pi*(ID_pb/2)**2
-    A_water = math.pi*((ID_w/2)**2-(OD_gas/2)**2)
-    Dh_w = ID_w - OD_gas
-    Dc = (ID_gas+OD_pb)/2
-
-    return T_1, T_2, A_pb, A_water, Dc, Dh_w
-H2O_d = []
-'''
-for m, t in zip(Final['OD_Pb'], Final['LBE Name']):
-    Pb_d = Total.loc[Total['Outer']==m]
-    Pb_d1.append(Pb_d.loc[Total['Name']==t])
-Pb_df = pd.concat(Pb_d1)
-'''
 
 
-for H2O_ID, w_name in zip(Total['Inner'], Total['Name']):
-    W_gap.append((H2O_ID - Final['OD_gas'])/2)
-    l = [w_name]*len(Final['OD_gas'])
-    w = H2O_ID*np.ones(len(Final['OD_gas']))
-    H2O_name.append(l)
-    H2O_d.append(w)
-    combo.append(Final)
-
-h2o = pd.DataFrame([np.concatenate(W_gap), np.concatenate(H2O_name), np.concatenate(H2O_d)], index = ['T_w', 'W name', 'ID_w']).T
+h2o, last = tube_geometry.water_dia(Total, Final)
  
-last = pd.concat([h2o, pd.concat(combo, ignore_index=True)], axis = 1)
-
+#This is the final collection of the diameters and velocity
 last = last.loc[last['ID_w']> last['OD_gas']]
-#last = last.loc[last['ID_Pb']>0.04]
 
-'''
-#gas gap name
-for m1, t1 in zip(Final['Gas Name'], Final['Gauge']):
-    Gas_d = Total.loc[Total['Name']==m1]
-    Gas_d1.append(Gas_d.loc[Total['Gauge']==t1])
-
-    #print(Gas_d)
-Gas_df = pd.concat(Gas_d1)
-'''
-#print(last)
-T_1, T_2, Area_pb, A_water, Dc, Dh_w = area_calcs(last['ID_Pb'], 
+#Temperatures and geometry calcs for Q calculations
+T_1, T_2, Area_pb, A_water, Dc, Dh_w = tube_geometry.area_calcs(last['ID_Pb'], 
                 last['OD_Pb'], last['ID_gas'], last['OD_gas'], last['ID_w'])
 
-areas = (pd.concat([T_1, T_2, Area_pb, A_water, Dc, Dh_w], axis=1)).rename(columns={0:'T1', 1:'T2', 'ID_Pb':'A_Pb',
-                                                                 2:'A_water', 3:'Dc', 4:'Dh_w'})
+areas = (pd.concat([T_1, T_2, Area_pb, A_water, Dc, Dh_w], 
+                   axis=1)).rename(columns={0:'T1', 1:'T2', 'ID_Pb':'A_Pb',
+                                            2:'A_water', 3:'Dc', 4:'Dh_w'})
 last = pd.concat([last, areas], axis = 1)
 
 
-def delta_T(MFR, T_hout, Q_required, T_roomtemp, delta_Tcold):
-    cp_hot = 164.8-3.94E-2*T_hout+1.25E-5*T_hout**2-4.56E5*T_hout**-2
-    delta_Thot = Q_required/(cp_hot*MFR)
-    
-    #final temps
-    T_hin = T_hout + delta_Thot
-    T_cin = T_roomtemp+273 #K
-
-    T_cout = T_cin + delta_Tcold
-
-    
-
-    return delta_Thot, T_hin, T_cin, T_cout
-
-def thermo_prop(T):
-    #calculations of thermophysical props
-    #LBE
-    rho_hot = -1.2046*T + 10989 #kg/cm^3
-    cp_hot = 164.8-3.94E-2*T+1.25E-5*T**2-4.56E5*T**-2
-    k_hot = 3.284 + 1.612E-2*T - 2.305E-6*T**2 #thermal conductivity
-    DV_hot = 4.94E-4*math.exp(754.1/T) #dynamic viscosity
-    KV_hot = DV_hot/rho_hot
-    return rho_hot, cp_hot, k_hot, DV_hot, KV_hot
 
 #dia = pd.concat([last, Dh_pb], axis =1)
 
@@ -204,15 +100,16 @@ T_hout = 200+273 #K
 T_cin = 20 #C
 Q_req = 100E3 #W
 delta_Tcold = 10 #K
-delta_Thot, T_hin, T_cin, T_cout = delta_T(LBE_MFR, T_hout, Q_req, T_cin, delta_Tcold)
+delta_Thot, T_hin, T_cin, T_cout = tube_geometry.delta_T(LBE_MFR, T_hout, Q_req, T_cin, delta_Tcold)
 
 print('The required temperature decrease is approximatly', delta_Thot)
 
 T_avg = (T_hin + T_hout)/2
 
-rho_hot, cp_hot, k_hot, DV_hot, KV_hot = thermo_prop(T_avg)
+#Properties of the LBE
+rho_hot, cp_hot, k_hot, DV_hot, KV_hot = tube_geometry.thermo_prop(T_avg)
 
-#H2O
+#H2O properties 
 rho_cold = 1000 #kg/cm^3
 cp_cold = 4180 #specific heat water J/kg K
 k_cold = 0.60694 #thermal conductivity
@@ -242,15 +139,14 @@ def LMTD(T_hotin, T_hotout, T_coldin, T_coldout):
 LBE_V = 1 #m/s
 max_h2oV = 3 #m/s
 H2O_V = velocity(rho_cold, A_water, w_MFR)
-MFR= MFR(rho_hot, Area_pb, LBE_V)
-last = pd.concat([last, H2O_V], axis = 1).rename(columns={0:'W_velocity'})
-#last = last.loc[last['W_velocity']<max_h2oV]
+MFR= pd.DataFrame([MFR(rho_hot, Area_pb, LBE_V)], index=['LBE mass flow']).T
+last = pd.concat([last, H2O_V, MFR], axis = 1).rename(columns={0:'W_velocity'})
 
 #heat capacity 
-C_LBE = cp_hot*MFR
+C_LBE = cp_hot*MFR.rename(columns={'LBE mass flow':0})
 C_w = cp_cold*w_MFR
 
-    
+#Calculated cold out temp and ideal Q    
 T_cout = (C_LBE/C_w)*(T_hin-T_hout)+T_cin
 Q_hot = C_LBE*(T_hin - T_hout)
 Q_cold = C_w*(T_cout - T_cin)
@@ -274,21 +170,28 @@ Nu_w = 0.023 * (Rey_w**0.8)*(Pr_w**0.33)
 #heat transfer W/m^2 K) 
 h_hot = Nu_LBE*(k_hot/last['ID_Pb'])
 h_cold = Nu_w*(k_cold/last['Dh_w'])
+
 #pipe thermal conductivities W/m K
 k_316 = 15 
 k_He = 0.189
 k_Ar = 0.0335 
 
 U=1/(1/h_hot +last['T1']/k_316 +last['GasGap']/k_He +last['T2']/k_316 +1/h_cold)
-#U = pd.DataFrame(U)
+U = pd.DataFrame(U)
 #LMTD method
 
 LMTD = LMTD(T_hin, T_hout, T_cin, T_cout)
+last=pd.concat([last, U, LMTD], axis = 1)
+last.to_csv(r'original_HX.csv')
+Dc = pd.DataFrame(last['Dc'].reset_index(drop=True))
 
-Ac_LMTD = Q_hot/(U*LMTD)
-Lc_LMTD = Ac_LMTD/(math.pi*last['Dc'])
+Ac_LMTD = (Q_hot/(U*LMTD)).reset_index(drop=True)
+Lc_LMTD = Ac_LMTD[0]/(math.pi*Dc['Dc'])
 #C_w = rho_cold*A_water*H2O_V*cp_cold
-one_M = max((last['Dc']*math.pi)*U*LMTD)
+U1 = U.reset_index(drop=True)
+LMTD = LMTD.reset_index(drop = True)
+one_M = ((1*Dc['Dc']*math.pi)*U1[0]*LMTD[0])
+QLMTD_max = max(one_M)
 
 #e-NTU method
 
@@ -298,7 +201,7 @@ Max= []
 Eff = []
 
 
-for h in C_LBE:
+for h in C_LBE[0]:
     if h < C_w:
         C_min = h
         C_max = C_w
@@ -320,65 +223,60 @@ e = pd.DataFrame(np.concatenate([Eff]))
   
 C_ratio = C_min/C_max
     
+#values for e-ntu
+data_calor = pd.DataFrame(np.array([[0, 0, 0, 0, 0, 0], 
+     [0.25, 0.221, 0.216, 0.21, 0.206, 0.205],
+     [0.5, 0.393, 0.378, 0.362, 0.35, 0.348],
+     [0.75, 0.528, 0.502, 0.477, 0.457, 0.452],
+     [1, 0.632, 0.598, 0.565, 0.538, 0.532],
+     [1.25, 0.713, 0.675, 0.635, 0.603, 0.595],
+     [1.5, 0.777, 0.735, 0.691, 0.655, 0.645]]),
+    index = [0, 0.25, 0.5, 0.75, 1, 1.25,
+    1.5], columns = ['NTU', 0, 0.25, 0.5, 0.7, 0.75])
+index_calor = [0, 0.25, 0.5, 0.7, 0.75]
+
+data_Ubend = pd.DataFrame(np.array([[0, 0, 0, 0, 0, 0], 
+     [0.25, 0.221, 0.215, 0.209, 0.204, 0.198],
+     [0.5, 0.393, 0.375, 0.357, 0.340, 0.324],
+     [0.75, 0.528, 0.494, 0.463, 0.434, 0.407],
+     [1.00, 0.632, 0.584, 0.540, 0.500, 0.463],
+     [1.25, 0.714, 0.652, 0.597, 0.546, 0.500],
+     [1.5, 0.777, 0.705, 0.639, 0.579, 0.526]]),
+    index = [0.0, 0.25, 0.5, 0.75, 1, 1.25,
+    1.5], columns = ['NTU', 0.0, 0.25, 0.5, 0.75, 1.00])
+index_ubend = [0.0, 0.25, 0.5, 0.75, 1.00]
 #NTU calculations
-def interp_NTU(e_new, C_new):
-    data = pd.DataFrame(np.array([[0, 0, 0, 0, 0, 0], 
-         [0.25, 0.221, 0.216, 0.21, 0.206, 0.205],
-         [0.5, 0.393, 0.378, 0.362, 0.35, 0.348],
-         [0.75, 0.528, 0.502, 0.477, 0.457, 0.452],
-         [1, 0.632, 0.598, 0.565, 0.538, 0.532],
-         [1.25, 0.713, 0.675, 0.635, 0.603, 0.595],
-         [1.5, 0.777, 0.735, 0.691, 0.655, 0.645]]),
-        index = [0, 0.25, 0.5, 0.75, 1, 1.25,
-        1.5], columns = ['NTU', 0, 0.25, 0.5, 0.7, 0.75])
-    index = [0, 0.25, 0.5, 0.7, 0.75]
-
-    
-    lower_c = max([t for t in index if t < C_new])
-
-
-    higher_c = min([t for t in index if t > C_new])
-
-    lower_e1 = max([t for t in data[lower_c] if t < e_new])
-
-    higher_e1 = min([t for t in data[lower_c] if t > e_new])
-    
-    lower_e2 = max([t for t in data[higher_c] if t < e_new])
-    higher_e2 = min([t for t in data[higher_c] if t > e_new])
-
-    #calc the interpolated e's
-    x1 = [lower_e1, lower_e2]
-    x2 = [higher_e1, higher_e2]
-    C = [lower_c, higher_c]
-    e_new_low = np.interp(C_new, C, x1)
-    e_new_high = np.interp(C_new, C, x2)
-    
-    
-    #calc interpolated NTU
-    lower_NTU = float(data['NTU'][data[lower_c] == lower_e2])
-    higher_NTU = float(data['NTU'][data[higher_c] == higher_e2])
-    
-    x3 = [lower_NTU, higher_NTU]
-    e = [e_new_low, e_new_high]
-    NTU = np.interp(e_new, e, x3)
-
-
-        
-    return NTU
 
 NTU =[]
 for ti, tu in zip(C_ratio[0], e[0]):
     
-    NTU.append(interp_NTU(tu, ti))
+    NTU.append(tube_geometry.interp_NTU(tu, ti, data_calor, index_calor))
 NTU_e = pd.DataFrame(np.concatenate([NTU]))
-U1 = pd.concat([U], ignore_index = True)
-Ac_NTU = (NTU_e*C_min)/U1
-Dc = pd.DataFrame(last['Dc'].reset_index(drop=True))
-Lc_NTU = Ac_NTU[0]/(math.pi*Dc['Dc'])
-print(min(Lc_NTU))
-print(Lc_LMTD)
 
+Ac_NTU = (NTU_e*C_min)/U1
+one_M_NTU=((1*Dc['Dc']*math.pi)*U1[0])/C_min[0]
+
+Lc_NTU = Ac_NTU[0]/(math.pi*Dc['Dc'])
+print(Lc_NTU)
+print(Lc_LMTD)
+e_calc = []
+for tie, tue in zip(C_ratio[0], one_M_NTU):
+    e_calc.append(tube_geometry.interp_e(tue, tie, data_calor, index_calor))
     
+e_calc_new = pd.DataFrame(np.concatenate([e_calc]))
+Q_NTU_one = e_calc_new*C_min*(T_hin-T_cout.reset_index(drop=True))
+QNTU_max = max(Q_NTU_one[0])
+'''
+v_w = 1.00177E-6 #m^3 / g at 20 C
+#pressure loss early of water side
+P = ((((Rey_w*DV_cold)/last['Dh_w'])**2)/(2*9.81))*v_w*(0.046/Rey_w**0.2)*(Lc_LMTD/(last['Dh_w']/4))
+HL_w = (0.046/Rey_w**0.2)*(Lc_LMTD/last['Dh_w'])*((last['W_velocity']**2)/(2*9.81))
+HL_LBE = (0.046/(Rey_LBE**0.2))*(Lc_LMTD/last['ID_Pb'])*((1**2)/(2*9.81))
+
+HL_oneM = (0.046/(Rey_LBE**0.2))*(1/last['ID_Pb'])*((1**2)/(2*9.81))
+
+
+
 lengths_and_dia = pd.concat([last.reset_index(drop=True), Lc_LMTD.reset_index(drop=True), Lc_NTU], axis=1)
 
 #for U-tube calculations
@@ -390,5 +288,7 @@ Fg = (((R**2 + 1)**0.5)*np.log((1-S)/(1-R*S)))/((R-1)*np.log(2-S*(R+1-(R**2+1)**
 U_Ubend=1/(1/h_hot +last['T1']/k_316 +1/h_cold)
 U_Ac_LMTD = Q_hot/(U_Ubend*LMTD*Fg)
 U_Lc_LMTD = U_Ac_LMTD/(math.pi*last['Dc'])
+'''
+
 
 
